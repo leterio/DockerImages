1556b5f4aacaf94a8bf5de104354703dfb543e62
Vinicius Leterio <viniciusleterio@gmail.com>
2022-11-24 16:50:01 -0300

    WSDD @ Adicionado mapeamento de interface de rede

--- wsdd/wsdd.sh
+++ wsdd/wsdd.sh
@@ -24,7 +24,7 @@ set -o nounset
 # Return: Formatted message on output
 log() {
     local message="${1:-""}"
-    [[ $message ]] && echo "${0##*/}: $message"
+    [[ $message ]] && echo -e "${0##*/}: $message"
 }
 
 ### usage: Help
@@ -43,6 +43,8 @@ WSDD_HOSTNAME   -n \"<hostname>\"
                         Override (NetBIOS) hostname to be used (default hostname)
 WSDD_WORKGROUP  -w \"<workgroup>\"
                         Set workgroup name (default WORKGROUP)
+WSDD_IFACE      -i \"<interface>\"
+                        Set the network interface for WSDD
 " >&2
     exit $RC
 }
@@ -50,13 +52,15 @@ WSDD_WORKGROUP  -w \"<workgroup>\"
 wsdd_domain=${WSDD_DOMAIN:-""}
 wsdd_hostname=${WSDD_HOSTNAME:-""}
 wsdd_workgroup=${WSDD_WORKGROUP:-""}
+wsdd_iface=${WSDD_IFACE:-""}
 
-while getopts ":hd:n:w:" opt; do
+while getopts ":hd:n:w:i:" opt; do
     case "$opt" in
         h) usage ;;
         d) wsdd_domain="${OPTARG}" ;;
         n) wsdd_hostname="${OPTARG}" ;;
         w) wsdd_workgroup="${OPTARG}" ;;
+        i) wsdd_iface="${OPTARG}" ;;
         "?") log "Unknown option: -$OPTARG"; usage 1 ;;
         ":") log "No argument value for option: -$OPTARG"; usage 2 ;;
     esac
@@ -66,9 +70,11 @@ shift $(( OPTIND - 1 ))
 if ps | grep [p]ython | grep -q wsdd.py; then
     log "Service already running, please restart container to apply changes"
 else
-    log "Launching WSDD"
+    log "Launching WSDD${wsdd_hostname:+\n\tHostname: "$wsdd_hostname"}${wsdd_domain:+\n\tDomain: "$wsdd_domain"}${wsdd_workgroup:+\n\tWorkgroup: "$wsdd_workgroup"}${wsdd_iface:+\n\tInterface: "$wsdd_iface"}"
+
     exec python /wsdd.py \
          ${wsdd_domain:+-d "$wsdd_domain"} \
          ${wsdd_hostname:+-n "$wsdd_hostname"} \
-         ${wsdd_workgroup:+-w "$wsdd_workgroup"}
+         ${wsdd_workgroup:+-w "$wsdd_workgroup"} \
+         ${wsdd_iface:+-i "$wsdd_iface"}
 fi
