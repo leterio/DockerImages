f788721f28fc0d27faa7355879fc5633e2d64806
Vinicius Leterio <viniciusleterio@gmail.com>
2023-06-14 13:37:08 -0300

    Minecraft Server @ Ajustado script

--- minecraft-server/start.sh
+++ minecraft-server/start.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env /bin/sh
 
 EXPECTED_JAR="/minecraft/server.jar"
+EXPECTED_RUN="/minecraft/run.sh"
+
 UID=${UID:-1000}
 GID=${GID:-1000}
 
-if [[ ! -f "$EXPECTED_JAR" ]]; then
-    echo "Missing $EXPECTED_JAR"
-    echo "Put the jar file in the volume with the name \"server.jar\""
+if [[ ! -f "$EXPECTED_JAR" ]] && [[ ! -f "$EXPECTED_RUN" ]]; then
+    echo "Missing $EXPECTED_JAR OR $EXPECTED_RUN"
     return;
 fi
 
@@ -14,10 +15,20 @@ echo "Setting UID(${UID}) and GID(${GID})"
 usermod -o -u $UID minecraft > /dev/null
 groupmod -o -g $GID minecraft > /dev/null
 
+if [[ ! -z $MINECRAFT_EULA ]]; then
+    echo "Accepting EULA ..."
+    echo "eula=true" > /minecraft/eula.txt
+fi
+
 echo "Fixing permissions"
 chown -R minecraft:minecraft /minecraft > /dev/null
 find /minecraft -type d -exec chmod 0750 {} \; > /dev/null
 find /minecraft -type f -exec chmod 0640 {} \; > /dev/null
 
 echo "Launching Minecraft Server"
-exec su-exec minecraft /bin/sh -c "cd /minecraft; java ${JAVA_ARGS} -jar \"${EXPECTED_JAR}\" nogui"
+if [[ -f "$EXPECTED_RUN" ]]; then
+    chmod +x $EXPECTED_RUN
+    exec su-exec minecraft /bin/sh -c "cd /minecraft; $EXPECTED_RUN"
+else 
+    exec su-exec minecraft /bin/sh -c "cd /minecraft; java ${JAVA_ARGS} -jar \"${EXPECTED_JAR}\" nogui"
+fi
